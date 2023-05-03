791e5479794c8750480694f02c63bc32356e4e06
LJH <hosboy93@naver.com>
2023-05-03 15:35:29 +0900

    feat($Program) : DAO 객체 생성

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,21 @@
+import DAO.DeptDAO;
+import DAO.EapplyDAO;
+import DAO.EmpDAO;
+import DAO.ErankDAO;
+import DAO.EstateDAO;
+import DAO.HolidayDAO;
+import DAO.Rest_holidayDAO;
 
 public class Program {
 	public void run() {
+		DeptDAO dept = new DeptDAO();
+		EapplyDAO eapply = new EapplyDAO();
+		EmpDAO emp = new EmpDAO();
+		ErankDAO erank = new ErankDAO();
+		EstateDAO estate = new EstateDAO();
+		HolidayDAO holiday = new HolidayDAO();
+		Rest_holidayDAO rest = new Rest_holidayDAO();
+		
 		
 	}
 }
