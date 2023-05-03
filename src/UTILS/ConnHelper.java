a33cd561fb1f9dcc5697bead8f95187469cc6735
LJH <hosboy93@naver.com>
2023-05-03 15:18:28 +0900

    feat($ConnHelper) : Connection 자동생성

--- src/UTILS/ConnHelper.java
+++ src/UTILS/ConnHelper.java
@@ -1,5 +1,20 @@
 package UTILS;
 
-public class ConnHelper {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class ConnHelper {	
+	public ConnHelper() { // 생성자 호출시 연결객체 생성
+		getConnection();
+	}
+	
+	private Connection getConnection() { // 연결객체
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "TEAM1", "1004");
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}		
+		return conn;
+	}
 }
