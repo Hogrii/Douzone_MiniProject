4f25191496c74bfa011c768e3f92de4301ca970b
LJH <hosboy93@naver.com>
2023-05-03 16:16:24 +0900

    feat($ConnHelper) : 자원해제(오버로딩) 추가

--- src/UTILS/ConnHelper.java
+++ src/UTILS/ConnHelper.java
@@ -2,6 +2,9 @@ package UTILS;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ConnHelper {
 	private static Connection conn;
@@ -15,5 +18,49 @@ public class ConnHelper {
 			System.out.println(e.getMessage());
 		}		
 		return conn;
+	}
+	
+	// Connection close
+	public static void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	// ResultSet close
+	public static void close(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	// Statement close
+	public static void close(Statement stmt) {
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	// PreparedStatement close
+	public static void close(PreparedStatement pstmt) {
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
 	}
 }
